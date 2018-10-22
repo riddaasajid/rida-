412e2432140bcfe41c8b8b878ee89c63358bd1e5
riddaasajid <38830551+riddaasajid@users.noreply.github.com>
2018-10-22 18:59:20 +0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,11 @@ void main()
 printf("\n HELLO WORLD \n ");
   printf("\n NAME: RIDA SAJID \n");
   printf("\n Roll #: bcs-f15-01 \n");
+  printf("\n Roll #: bcs-f15-01 \n");
+  printf("\n Roll #: bcs-f15-01 \n");
+  printf("\n Roll #: bcs-f15-01 \n");
+  printf("\n Roll #: bcs-f15-01 \n");
+ 
    printf("\n university of punjab \n");
 
 }
